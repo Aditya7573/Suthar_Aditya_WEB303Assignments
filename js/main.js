d511dcb498afccbdea217d583b058b26608088db
Aditya7573 <134808390+Aditya7573@users.noreply.github.com>
2023-09-19 00:33:09 -0400

    Assignment 2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
+//WEB303 Assignment 2
+// Name : Aidtya Suthar
+// Date : 19/09/2023
+// 0835715
+
+// Wait for the document to be fully loaded before executing JavaScript
 $(document).ready(function() {
+    // Attach a click event listener to elements with the IDs 'prospect', 'convert', and 'retain'
     $("#prospect, #convert, #retain").on("click", function(event) {
+        // Prevent the default behavior of the clicked link (e.g., navigating to a new page)
         event.preventDefault();
 
+        // Get the ID of the clicked link ('prospect', 'convert', or 'retain')
         var linkId = $(this).attr("id");
 
+        // Make an AJAX request to load content from a file with a name matching the link ID
         $.ajax({
-            url: linkId + ".html",
-            type: 'GET', // Add this line to specify the request type
-            dataType: "html",
+            url: linkId + ".html", // Construct the URL based on the link ID
+            type: 'GET', // Specify the HTTP request method as 'GET'
+            dataType: "html", // Specify that we expect HTML content in the response
             success: function(data) {
+                // Get a reference to the 'content' div
                 var $contentVal = $('#content');
-                $contentVal.hide().html(data).fadeIn(1500).css({
+
+                // Hide the content div with a 400ms animation
+                $contentVal.hide();
+
+                // Set the HTML content of the 'content' div to the data received from the AJAX request
+                $contentVal.html(data);
+
+                // Fade in the 'content' div with a 1500ms animation
+                $contentVal.fadeIn(1500);
+
+                // Apply a border to the 'content' div
+                $contentVal.css({
                     'border': '1.5px solid blue'
                 });
             },
             error: function() {
+                // Display an alert message if there is an error loading content
                 alert("Error loading content.");
             }
         });
