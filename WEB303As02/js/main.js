53c6ce5a587872b363247f5cb73ada60976f9230
Aditya7573 <134808390+Aditya7573@users.noreply.github.com>
2023-09-19 00:15:02 -0400

    Add files via upload

--- WEB303As02/js/main.js
+++ WEB303As02/js/main.js
@@ -1,27 +1,19 @@
-// Add a click event listener to the links in the content-wrapper
 $(document).ready(function() {
     $("#prospect, #convert, #retain").on("click", function(event) {
         event.preventDefault();
 
-        // Get the ID of the clicked link
         var linkId = $(this).attr("id");
 
-        // Perform an AJAX request to load content from the corresponding file
         $.ajax({
             url: linkId + ".html",
             dataType: "html",
             success: function(data) {
-                // Hide the content div with animation
                 $("#content").hide(400, function() {
-                    // Clear the existing content
                     $(this).empty();
-
-                    // Append the new content and show it with animation
                     $(this).html(data).show(400);
                 });
             },
             error: function() {
-                // Handle error, e.g., content not found
                 alert("Error loading content.");
             }
         });
