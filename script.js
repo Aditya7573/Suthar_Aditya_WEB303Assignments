83191276014a2051b916decfade13f6350ce88fd
Aditya7573 <134808390+Aditya7573@users.noreply.github.com>
2023-10-23 22:49:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-$(document).ready(function () {
-    // Create an array of ContentItem objects with the theme "F1"
+// ContentItem class
+class ContentItem {
+    constructor(id, name, description, categoryGenre) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.categoryGenre = categoryGenre;
+    }
+  
+    updateContentItem(id, name, description, categoryGenre) {
+      if (id === this.id) {
+        if (name !== null) this.name = name;
+        if (description !== null) this.description = description;
+        if (categoryGenre !== null) this.categoryGenre = categoryGenre;
+      }
+    }
+  
+    toString() {
+      return `
+        <div id="content-item-${this.id}" class="content-item-wrapper">
+          <h2>${this.name}</h2>
+          <p>${this.description}</p>
+          <div>${this.categoryGenre}</div>
+        </div>
+      `;
+    }
+  }
+  
+  $(document).ready(function () {
+    // Create an array of ContentItem objects
     const contentItems = [
-      new ContentItem(0, "Lewis Hamilton", "Seven-time world champion", "Driver"),
-      new ContentItem(1, "Monaco Grand Prix", "Iconic street circuit", "Race"),
-      new ContentItem(2, "Ferrari SF90", "2019 Scuderia Ferrari car", "Car"),
-      new ContentItem(3, "Red Bull Racing", "Austrian racing team", "Team"),
-      new ContentItem(4, "Silverstone Circuit", "British Grand Prix venue", "Race"),
+      new ContentItem(0, "Content 1", "Description 1", "Category 1"),
+      new ContentItem(1, "Content 2", "Description 2", "Category 2"),
+      new ContentItem(2, "Content 3", "Description 3", "Category 3"),
+      new ContentItem(3, "Content 4", "Description 4", "Category 4"),
+      new ContentItem(4, "Content 5", "Description 5", "Category 5"),
     ];
   
     // Add content items to the page
@@ -17,7 +45,7 @@ $(document).ready(function () {
     // Bonus: Add buttons to update content items
     $('#updateSuccessButton').click(function () {
       // Example of a successful update
-      contentItems[0].updateContentItem(0, "Updated Lewis Hamilton", null, null);
+      contentItems[0].updateContentItem(0, "Updated Content 1", null, null);
       // Re-render the updated content
       $contentItemList.empty();
       contentItems.forEach((item) => {
