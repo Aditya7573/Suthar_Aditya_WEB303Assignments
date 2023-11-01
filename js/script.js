3ea3867bc5d89c2aa3717521be39b9c1542b288e
Aditya7573 <134808390+Aditya7573@users.noreply.github.com>
2023-11-01 00:16:13 -0400

    assignment-7

--- js/script.js
+++ js/script.js
@@ -1,29 +1,14 @@
 $(document).ready(function () {
-    // Accordion functionality
-    $(".accordion-header").click(function () {
-        $(this).next(".accordion-content").slideToggle();
-        $(".accordion-content").not($(this).next()).slideUp();
-    });
+    let charactersData = [];
+    let sortedColumn = null;
+    let sortOrder = 1;
 
-    // Tabbed section functionality
-    $(".tab-headers li").click(function () {
-        var index = $(this).index();
-        $(".tab-content > div").hide().eq(index).show();
-        $(".tab-headers li").removeClass("active");
-        $(this).addClass("active");
-    });
+    // Function to render the table based on character data
+    function renderTable() {
+        const tbody = $("#characterTable tbody");
+        tbody.empty();
 
-    $(document).ready(function () {
-        let charactersData = [];
-        let sortedColumn = null;
-        let sortOrder = 1;
-      
-        // Function to render the table based on character data
-        function renderTable() {
-          const tbody = $("#characterTable tbody");
-          tbody.empty();
-      
-          charactersData.forEach(function (character) {
+        charactersData.forEach(function (character) {
             const row = $("<tr>");
             row.append($("<td>").text(character.firstName));
             row.append($("<td>").text(character.lastName));
@@ -33,58 +18,57 @@ $(document).ready(function () {
             row.append($("<td>").text(character.dateOfBirth));
             row.append($("<td>").text(character.debutDate));
             tbody.append(row);
-          });
-        }
-      
-        // Function to update table header with arrow symbols
-        function updateHeaderArrows() {
-          $("#characterTable th .sort-icon").html(""); // Clear existing icons
-          $("#characterTable th").each(function () {
+        });
+    }
+
+    // Function to update table header with arrow symbols
+    function updateHeaderArrows() {
+        $("#characterTable th .sort-icon").html(""); // Clear existing icons
+        $("#characterTable th").each(function () {
             if ($(this).data("sort") === sortedColumn) {
-              if (sortOrder === 1) {
-                $(this).find(".sort-icon").html("&#x25B2;"); // Up arrow
-              } else {
-                $(this).find(".sort-icon").html("&#x25BC;"); // Down arrow
-              }
+                if (sortOrder === 1) {
+                    $(this).find(".sort-icon").html("&#x25B2;"); // Up arrow
+                } else {
+                    $(this).find(".sort-icon").html("&#x25BC;"); // Down arrow
+                }
             }
-          });
-        }
-      
-        // Function to sort the table by a given column
-        function sortTable(column) {
-          if (sortedColumn === column) {
+        });
+    }
+
+    // Function to sort the table by a given column
+    function sortTable(column) {
+        if (sortedColumn === column) {
             sortOrder *= -1;
-          } else {
+        } else {
             sortedColumn = column;
             sortOrder = 1;
-          }
-      
-          charactersData.sort((a, b) => {
+        }
+
+        charactersData.sort((a, b) => {
             const aValue = a[column];
             const bValue = b[column];
             if (aValue < bValue) {
-              return -1 * sortOrder;
+                return -1 * sortOrder;
             }
             if (aValue > bValue) {
-              return 1 * sortOrder;
+                return 1 * sortOrder;
             }
             return 0;
-          });
-      
-          renderTable();
-          updateHeaderArrows();
-        }
-      
-        // Handle click on table headings for sorting
-        $("#characterTable th").click(function () {
-          const column = $(this).data("sort");
-          sortTable(column);
         });
-      
-        // Load character data from the JSON file
-        $.getJSON("characters.json", function (data) {
-          charactersData = data;
-          renderTable();
-        });
-      });
-    })      
+
+        renderTable();
+        updateHeaderArrows();
+    }
+
+    // Handle click on table headings for sorting
+    $("#characterTable th").click(function () {
+        const column = $(this).data("sort");
+        sortTable(column);
+    });
+
+    // Load character data from the JSON file using AJAX
+    $.getJSON("js/characters.json", function (data) {
+        charactersData = data;
+        renderTable();
+    });
+});
