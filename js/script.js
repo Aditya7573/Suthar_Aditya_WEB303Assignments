94c8b4e788d6ebd808ac6c11e1f3fd9587703bab
Aditya7573 <134808390+Aditya7573@users.noreply.github.com>
2023-11-17 01:16:17 -0500

    assignment-9

--- js/script.js
+++ js/script.js
@@ -1,85 +1,79 @@
-$(document).ready(function (){
+$(document).ready(function () {
     $.ajax({
-        url: "character.json",
-        success: function(data) {
-            data.Anime.forEach(Anime => {
-                $("table tbody").append(`
-                <tr class="name">
-                <td>${Anime.First_Name}</td>
-                <td>${Anime.Last_Name}</td>
-                <td>${Anime.Role}</td>
-                <td>${Anime.Affiliation}</td>
-                <td>${Anime.Status}</td>
-                <td>${Anime.FirstAppearance}</td>
-                <td>${Anime.Abilities}</td>
-                <td>${Anime.NotableAchievements}</td>
-                </tr>
-                `);
-            
-                let data1 = 0;
-                let data2 = 0;
-                let First_Name = Anime.First_Name[0].toLowerCase();
-                if (First_Name.match(/[a-mA-M]/)) {
-                    data1++;
-                } else {
-                    data2++;
-                }
-            });
+      url: "js/character.json",
+      success: function (data) {
+        data.cartoons.forEach((cartoon) => {
+          $("table tbody").append(`
+            <tr class="name">
+              <td>${cartoon.First_Name}</td>
+              <td>${cartoon.Last_Name}</td>
+              <td>${cartoon.Role}</td>
+              <td>${cartoon.Affiliation}</td>
+              <td>${cartoon.Status}</td>
+              <td>${cartoon.FirstAppearance}</td>
+              <td>${cartoon.Abilities}</td>
+              <td>${cartoon.NotableAchievements}</td>
+            </tr>
+          `);
+        });
+  
+        // Initialize filter counts
+        updateFilterCounts();
+      }
+    });
+  
+    $("#search").on("input", function () {
+      var searchTerm = $(this).val().toLowerCase();
+  
+      $("tbody tr.name").each(function () {
+        let firstName = $(this).find("td:eq(0)").text().toLowerCase();
+  
+        if (firstName.includes(searchTerm)) {
+          $(this).addClass("highlighted");
+        } else {
+          $(this).removeClass("highlighted");
         }
-    });      
-
-    $("#searchButton1").click(function(e){
-        $("#cartoons tr:not(:first-child)").filter(function (e) {
-            var valueData = $(this).find("td:eq(1)").text();
-            
-                if (valueData.includes("A") || valueData.includes("B") || valueData.includes("C") || valueData.includes("D") || valueData.includes("E") || valueData.includes("F") || valueData.includes("G") || valueData.includes("H") || valueData.includes("I") || valueData.includes("J") || valueData.includes("K") || valueData.includes("L") || valueData.includes("M")) {
-                    $(this).show();
-                }
-                else
-                {
-                    $(this).hide();
-                }
-                dataSave = $('tr:visible').length;
-
-                $('#button1').text(`( ${dataSave - 1} )`);
-                $('#button2').text("");
-        });
+      });
     });
-        
-    $("#searchButton2").click(function(e){ 
-        $("#cartoons tr:not(:first-child)").filter(function (e) {
-            var valueData = $(this).find("td:eq(1)").text();
-            
-            if (valueData.includes("O") || valueData.includes("P") || valueData.includes("Q") || valueData.includes("R") || valueData.includes("S") || valueData.includes("T") || valueData.includes("U") || valueData.includes("V") || valueData.includes("W") || valueData.includes("X") || valueData.includes("Y") || valueData.includes("Z")) {
-                $(this).show();
-            }
-            else
-            {
-                $(this).hide();
-            }
-            dataSave = $('tr:visible').length;
-
-            $('#button2').text(`( ${dataSave - 1} )`);
-            $('#button1').text("");
-        });
+  
+    $("#filterAM").click(function () {
+      filterByLastName("A", "M");
     });
-
-    $("#search").keydown(function(e){
-        $("#cartoons").each(function( index ) {
-            var rowData=$("tbody").find("tr").show();
-
-            $("tbody tr").each(function (td) {
-                let value = $(this).text().toLowerCase();
-
-                // console.log(value);
-                //console.log(rowData);
-
-                if(value.includes(e.target.value.toLowerCase())) {
-                    $(this).css("background-color", "DarkGreen");
-                } else {
-                    $(this).css("background-color", "");
-                }
-            }); 
-        });
+  
+    $("#filterNZ").click(function () {
+      filterByLastName("N", "Z");
     });
-});
+  
+    function filterByLastName(startLetter1, startLetter2) {
+      $("tbody tr.name").each(function () {
+        var lastName = $(this).find("td:eq(1)").text().charAt(0).toUpperCase();
+  
+        if (
+          (lastName >= startLetter1 && lastName <= startLetter2) ||
+          lastName === ""
+        ) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+  
+      updateFilterCounts();
+    }
+  
+    function updateFilterCounts() {
+      var countAM = $("tbody tr.name").filter(function () {
+        var lastName = $(this).find("td:eq(1)").text().charAt(0).toUpperCase();
+        return lastName >= "A" && lastName <= "M";
+      }).length;
+  
+      var countNZ = $("tbody tr.name").filter(function () {
+        var lastName = $(this).find("td:eq(1)").text().charAt(0).toUpperCase();
+        return lastName >= "N" && lastName <= "Z";
+      }).length;
+  
+      $("#filterAM").text(`A - M (${countAM})`);
+      $("#filterNZ").text(`N - Z (${countNZ})`);
+    }
+  });
+  
