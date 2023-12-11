d1ebf1dea4e01bdb5d10d1c36a2abc045a928973
Aditya7573 <134808390+Aditya7573@users.noreply.github.com>
2023-12-11 18:00:36 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,77 @@
-$(document).ready(function() {
-    // Accordion functionality
-    $(".accordion-header").click(function() {
-        $(this).next(".accordion-content").slideToggle();
-        $(".accordion-content").not($(this).next()).slideUp();
-    });
+$(document).ready(function () {
+    $.ajax({
+      url: "js/character.json",
+      success: function (data) {
+        data.cartoons.forEach((cartoon) => {
+          $("table tbody").append(`
+            <tr class="name">
+              <td>${cartoon.breed}</td>
+              <td>${cartoon.country}</td>
+              <td>${cartoon.origin}</td>
+              <td>${cartoon.coat}</td>
+              <td>${cartoon.partten}</td>
 
-    // Tabbed section functionality
-    $(".tab-headers li").click(function() {
-        var index = $(this).index();
-        $(".tab-content > div").hide().eq(index).show();
-        $(".tab-headers li").removeClass("active");
-        $(this).addClass("active");
+            </tr>
+          `);
+        });
+  
+        // Initialize filter counts
+        updateFilterCounts();
+      }
     });
-});
+  
+    $("#search").on("input", function () {
+      var searchTerm = $(this).val().toLowerCase();
+  
+      $("tbody tr.name").each(function () {
+        let firstName = $(this).find("td:eq(0)").text().toLowerCase();
+  
+        if (firstName.includes(searchTerm)) {
+          $(this).addClass("highlighted");
+        } else {
+          $(this).removeClass("highlighted");
+        }
+      });
+    });
+  
+    $("#filterAM").click(function () {
+      filterByLastName("A", "M");
+    });
+  
+    $("#filterNZ").click(function () {
+      filterByLastName("N", "Z");
+    });
+  
+    function filterByLastName(startLetter1, startLetter2) {
+      $("tbody tr.name").each(function () {
+        var lastName = $(this).find("td:eq(1)").text().charAt(0).toUpperCase();
+  
+        if (
+          (lastName >= startLetter1 && lastName <= startLetter2) ||
+          lastName === ""
+        ) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+  
+      updateFilterCounts();
+    }
+  
+    function updateFilterCounts() {
+      var countAM = $("tbody tr.name").filter(function () {
+        var lastName = $(this).find("td:eq(1)").text().charAt(0).toUpperCase();
+        return lastName >= "A" && lastName <= "M";
+      }).length;
+  
+      var countNZ = $("tbody tr.name").filter(function () {
+        var lastName = $(this).find("td:eq(1)").text().charAt(0).toUpperCase();
+        return lastName >= "N" && lastName <= "Z";
+      }).length;
+  
+      $("#filterAM").text(`A - M (${countAM})`);
+      $("#filterNZ").text(`N - Z (${countNZ})`);
+    }
+  });
+  
